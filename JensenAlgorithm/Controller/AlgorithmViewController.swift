828fd475190c0513d39b9f2bfb2a78ca49cba274
Jensen Judkins <thesmelliestman@gmail.com>
2018-10-23 08:42:30 -0600

    Initial commit

---
+++ JensenAlgorithm/Controller/AlgorithmViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Jensen Algorithm
+//
+//  Created by Judkins, Jensen on 10/23/18.
+//  Copyright © 2018 Judkins, Jensen. All rights reserved.
+//
+
+import UIKit
+
+class AlgorithmViewController: UIViewController
+{
+    @IBOutlet weak var algorithmText: UILabel!
+    
+    @IBOutlet weak var swiftImage: UIImageView!
+    
+    override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        formatAlgorithm()
+    }
+
+    override func didReceiveMemoryWarning() -> Void
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    
+    private func formatAlgorithm() -> Void
+    {
+        let stepOne : String = ""
+        let stepTwo : String = ""
+        let stepThree : String = ""
+    }
+
+}
+
