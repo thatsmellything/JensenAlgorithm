697a5a1d401fc852866b19f894c2722ab95b54e7
Jensen Judkins <thesmelliestman@gmail.com>
2018-10-23 09:56:22 -0600

    Fixed all bugs and put in the algorithm. Hwne tested it still doesnt work though so it still needs work

--- JensenAlgorithm/Controller/AlgorithmViewController.swift
+++ JensenAlgorithm/Controller/AlgorithmViewController.swift
@@ -8,20 +8,80 @@
 
 import UIKit
 
-class AlgorithmViewController: UIViewController
+public class AlgorithmViewController: UIViewController
 {
+    //Mark: Data memnbers
     @IBOutlet weak var algorithmText: UILabel!
+    
+    private func formatAlgorithm() -> Void
+    {
+        var algorithmSteps : [String] = []
+        
+        //mark: algorithm definitions
+        let algorithm : String = "How to do some swift"
+        
+        let stepOne : String = "First you do the hokey pokey"
+        let stepTwo : String = "this is sme more txt"
+        let stepThree : String = "pretty neet stuff"
+        //finish adding all steps
+        let algorythm = [stepOne, stepTwo, stepThree]
+        
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributedString(string: algorithm, attributes: attributesDictionary)
+        
+        for step in algorithmSteps
+        {
+            let bullet : String = "👻"
+            
+            let formattedStep : String = "\n\(bullet) \(step)"
+            let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
+            let outlineStyle = createOutlineStyle()
+            
+            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : outlineStyle], range: NSMakeRange(0,attributedStringStep.length))
+            fullAttributedString.append(attributedStringStep)
+            
+            
+        }
+        
+        algorithmText.attributedText = fullAttributedString
+    }
+    
+    
+    private func createOutlineStyle() -> NSParagraphStyle
+    {
+        let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        outlineStyle.alignment = .left
+        outlineStyle.defaultTabInterval = 15
+        outlineStyle.firstLineHeadIndent = 20
+        outlineStyle.headIndent = 35
+        
+        return outlineStyle
+    }
+
     
     @IBOutlet weak var swiftImage: UIImageView!
     
-    override func viewDidLoad() -> Void
+    public override func viewDidLoad() -> Void
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         formatAlgorithm()
     }
 
-    override func didReceiveMemoryWarning() -> Void
+    
+    private func createOutletStyle() -> NSParagraphStyle
+    {
+        let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        outlineStyle.alignment = .left
+        outlineStyle.defaultTabInterval = 15
+        outlineStyle.firstLineHeadIndent = 20
+        outlineStyle.headIndent = 35
+        
+        return outlineStyle
+    }
+
+    
+    public override func didReceiveMemoryWarning() -> Void
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -30,12 +90,6 @@ class AlgorithmViewController: UIViewController
     
     
     
-    private func formatAlgorithm() -> Void
-    {
-        let stepOne : String = ""
-        let stepTwo : String = ""
-        let stepThree : String = ""
-    }
-
+    
 }
 
